ed91e017f4f6ecfd6a50d0bc3452073446911d51
Joe Pennock <pennockjoe@gmail.com>
2020-08-22 11:10:48 -0700

    reworked file structure

--- javascript401/arrays/arrayReverse/__tests/array-reverse.test.js
+++ javascript401/arrays/arrayReverse/__tests/array-reverse.test.js
@@ -3,7 +3,11 @@
 const arrayReverse = require('../array-reverse');
 
 describe('Testing for arrayReverse function, JS 401 Code Challenge 01', () => {
-  test('Function should reverse the contents of an array regardless of the contents of said array.', () => {
-    expect(arrayReverse([1,2,3,4,5]).toStrictEqual([5,4,3,2,1]));
+  it('Function should reverse the contents of an array regardless of the contents of said array.', () => {
+    let input = [1,2,3,4,5,6,7,8];
+    let actual= [8,7,6,5,4,3,2,1];
+    let func = arrayReverse(input);
+
+    expect(func).toEqual(actual);
   });
 });
