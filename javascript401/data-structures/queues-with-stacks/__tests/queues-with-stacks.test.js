bf282fc0133872da12db66b7897e8c0964cbd09e
Joe Pennock <pennockjoe@gmail.com>
2020-09-15 22:06:05 -0700

    added whiteboard images for challenge 11 solution, fleshed out README with all relevent challenge information, added basic scaffold for PseudoQueue class

---
+++ javascript401/data-structures/queues-with-stacks/__tests/queues-with-stacks.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Queue = require('../../stacks-and-queues/lib/stack.js');
+
+class PseudoQueue {
+
+  constructor(){
+    this.stack1 = new Queue();
+    this.stack2 = new Queue();
+  };
+
+  enqueue(){
+    
+  };
+
+  dequeue(){
+
+  };
+
+};
+
+module.exports = PseudoQueue;
