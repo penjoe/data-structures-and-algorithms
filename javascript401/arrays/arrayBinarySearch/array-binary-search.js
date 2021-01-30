ef2a7da79f472b0454b95f4d19c2f6c9209324f1
Joe Pennock <pennockjoe@gmail.com>
2021-01-29 19:57:56 -0800

    hotfix to cc-03

--- javascript401/arrays/arrayBinarySearch/array-binary-search.js
+++ javascript401/arrays/arrayBinarySearch/array-binary-search.js
@@ -1,33 +1,18 @@
 'use strict'
 
-/*
-Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
-*/
-
 function binarySearch(arr, key) {
 
-  let min = 0; // first value in array
-  let max = arr.length - 1; // last value in array
+  let min = 0; 
+  let max = arr.length - 1;
 
-  //initialize while min does not meet max
-  while( min !== max ) {
-
-    //calculates middle of array
+  while( min <= max ) {
     let mid = Math.floor(( min + max ) / 2);
-
-    //logic to determine middle of array and compare mid to the key
     if ( arr[mid] === key ) {
-
-      return mid ;
-
+      return mid;
     } else if ( arr[mid] < key ) {
-
       min = mid + 1;
-
-    } else if ( arr[mid] > key ){ 
-
+    } else { 
       max = mid - 1;
-
     };
   };
   return -1;
